128c803adf5dde52dc387d742e9790c1a4ec42da
Akash Choudhary <akashkrchoudhary007@gmail.com>
2025-01-31 00:10:16 +0530

    +

--- pattern.java
+++ pattern.java
@@ -91,6 +91,65 @@ public class pattern {
 //     * * * 
 //       *
 
+/*
+
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Value of n: ");
+        int n = sc.nextInt();
+        int i , j;
+        int nsp = n-1;  //no of spaces
+        int nst = 1;    //no of spaces
+        for(i=1;i<=n;i++){
+            int a = 1;
+            for(j=1;j<=nsp;j++){
+                System.out.print(" " +" ");
+            }
+            for(j=1; j<=nst; j++){
+                System.out.print("*"+" ");
+                
+            }   
+            nsp--;
+            nst+=2; 
+            System.out.println();
+        }
+        // 2nd phase
+        nsp = 1;  //no of spaces
+        nst = nst-4;    //no of spaces
+        for(i=1;i<=n;i++){
+            int a = 1;
+            for(j=1;j<=nsp;j++){
+                System.out.print(" " +" ");
+            }
+            for(j=1; j<=nst; j++){
+                System.out.print("*"+" ");
+                
+            }   
+            nsp++;
+            nst-=2; 
+            System.out.println();
+        }
+        sc.close();
+        
+    }
+    
+}
+
+
+*/
+
+// Print The given patten
+//       *
+//     * * * 
+//   * * * * *
+// * * * * * * *
+//  * * * * * *
+//  * * * * * *
+//  * * * * * *
+//  * * * * * *
 import java.util.Scanner;
 
 public class pattern {
